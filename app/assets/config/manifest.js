fdbbd4da84246093ae74c0155c057d7c09499dda
Dmitry-Medvedev <codemedvedev@gmail.com>
2019-12-07 01:06:15 +0300

    Подключил bootstrap,добавил навигацию и стилизовал статические страницы

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,10 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+//= require jquery3
+//= require popper
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
